42ed04e010fbdd8e6ae3128a1b77e9be18141e7d
baesongil <simon.bae@altibase.com>
2022-07-01 10:40:39 +0900

    add generateToken function

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express=require('express')
 const app = express()
 const port=5000
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const config = require('./config/key');
 
@@ -11,16 +12,44 @@ const { User } = require("./models/User");
 app.use(bodyParser.urlencoded({extended: true}));
 // application/json
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require('mongoose')
-
+// mongodb connect
 mongoose.connect(config.mongoURI,function(err){
     if(err){
         console.error('mongodb connection error', err)
     }
-    console.log('mongodb connected')
+    else console.log('mongodb connected')
 })
 
+/*
+// postgresql connect
+var { Client } = require('pg');
+//const { postgresURI } = require('./config/dev');
+//console.log(postgresURI);
+
+const pg = new Client ({
+    user: "testuser",
+    host: "192.168.1.99",
+    database: "testdb",
+    password: "testuser",
+    port: 5432,
+});
+
+pg.connect(function(err){
+    if(err){
+        console.error('postgresql connection error', err)
+    }
+    else console.log('postgresql connected')
+})
+
+pg.query("select * from teacher",(err, res) => {
+    if(!err) console.log(res.rows);
+    else console.log(err);
+    pg.end();
+})
+*/
 
 app.get('/', (req, res) => res.send('Hello World!!  Have a good day!'))
 
@@ -37,6 +66,40 @@ app.post('/register',(req, res) => {
     })
 })
 
+app.post('/login', (req, res) => {
+    // 요청된 이메일을 데이터베이스에서 조회한다.
+    User.findOne({ email: req.body.email}, (err, user) => {
+        if(!user) {
+            return res.json({
+                loginSuccess: false,
+                message: "제공된 이메일에 해당하는 유저가 없습니다."
+            })
+        }
+
+        // 요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인.
+
+        user.comparePassword(req.body.password, (err, isMatch) => {
+
+            if(!isMatch) {
+                res.status(400)
+                res.json({loginSuccess: false, message:"비밀번호가 틀렸습니다"})  
+                return
+            } 
+                
+            // 비밀번호까지 맞다면 토큰을 생성하기.
+            user.generateToken((err, user) => {
+                if(err) {
+                    return res.status(400).send(err);
+                }
+
+                // 토큰을 쿠키에 저장한다. (쿠키 or local storage or session)
+                res.cookie("x_auth",user.token)
+                .status(200)
+                .json({loginSuccess: true, userId: user._id})
+            });
+        });
+    })
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port : ${port}` )
--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
      name: {
@@ -47,8 +48,37 @@ userSchema.pre('save', function( next ){
              next()
          })
       })
+   } else {
+      next()
    }
 })
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+   // PlainPassword 와 암호호된 비밀번호를 서로 비교
+   bcrypt.compare(plainPassword, this.password, function(err, isMatch)  {
+      if(err) {
+         return cb(err)
+      } else {
+         return cb(null, isMatch)
+      }
+   })
+}
+
+userSchema.methods.generateToken = function(cb) {
+   var user = this;
+   // jsonwebtoken 을 이용해서 token을 생성하기
+   var token = jwt.sign(user._id.toHexString(), 'secretToken')
+   user.token = token
+   user.save(function(err, user){
+      if(err) 
+      {
+         return cb(err)
+      }
+      else {
+         return cb(null, user)
+      }
+   })
+}
 
 const User = mongoose.model('User', userSchema)
 
